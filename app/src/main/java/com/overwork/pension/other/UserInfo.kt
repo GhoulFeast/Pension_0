09d445f61ade5a73bcb87e056a7f364839631615
hq <376954839@qq.com>
2018-07-06 15:21:34 +0800

    Add files via upload

---
+++ app/src/main/java/com/overwork/pension/other/UserInfo.kt
@@ -0,0 +1,11 @@
+package com.overwork.pension.other
+
+var userId=""
+var userType=""
+var userName=""
+var userLevel=""
+var userLevelName=""
+var entryTime=""
+var workingYears =""
+var superiorName=""
+var userPortrait=""
