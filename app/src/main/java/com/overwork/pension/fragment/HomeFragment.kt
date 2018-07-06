fd2daca3c06a666c475317650e00ff6cc5bc6d4e
hq <376954839@qq.com>
2018-07-06 15:36:43 +0800

    Add files via upload

--- app/src/main/java/com/overwork/pension/fragment/HomeFragment.kt
+++ app/src/main/java/com/overwork/pension/fragment/HomeFragment.kt
@@ -8,11 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.overwork.pension.R
 import com.overwork.pension.activity.MenuActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment :Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_home, container, false)
         (activity as MenuActivity).setTextView(R.string.ylyxt)
+        start_job.setOnClickListener{
+            (activity as MenuActivity).showFragment(TodayTaskFragment())
+        }
+        come_room.setOnClickListener{
+            
+        }
         return view
 
     }
