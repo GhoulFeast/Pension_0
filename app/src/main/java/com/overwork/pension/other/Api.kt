64e7e81d3cd76ff1293e63b1e3f370897cd45f27
wwan12 <376954839@qq.com>
2018-07-21 23:07:02 +0800

    v.21 更新测试地址

--- app/src/main/java/com/overwork/pension/other/Api.kt
+++ app/src/main/java/com/overwork/pension/other/Api.kt
@@ -2,18 +2,34 @@ package com.overwork.pension.other
 
 val BASEURL=""//基本
 
-val LOGIN = ""//登陆
-val MSGLIST = ""//消息列表
-val M_TASK = ""//明日任务
-val T_TASK = ""//今天任务
-val T_ABNORMAL = ""//交班——异常信息
-val T_HANDOVERINFO = ""//交班——交班信息
-val OVER_TASK = ""//完成任务
-val TOMORROW_TASK = ""//明日任务
-val ABNORMALITY = ""//异常情况提交
-val THIS_TIME_TASK = ""//某一时段护理任务
-val T_HANDOVERDIRECTOR = ""//交班-主管
-val AUTO_UPDATE_MSG=""//新消息
-val MSGLIST_READ = ""//消息——读
+//val LOGIN = ""//登陆
+//val MSGLIST = ""//消息列表
+//val T_TASK = ""//今天任务
+//val T_ABNORMAL = ""//交班——异常信息
+//val T_HANDOVERINFO = ""//交班——交班信息
+//val OVER_TASK = ""//完成任务
+//val TOMORROW_TASK = ""//明日任务
+//val ABNORMALITY = ""//异常情况提交
+//val THIS_TIME_TASK = ""//某一时段护理任务
+//val T_HANDOVERDIRECTOR = ""//交班-主管
+//val AUTO_UPDATE_MSG=""//新消息
+//val MSGLIST_READ = ""//消息——读
+//
+//val ROOM_LIST = ""//房间列表
 
-val ROOM_LIST = ""//房间列表
+
+val LOGIN = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/login.json"//登陆
+val MSGLIST = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/msgList.json"//消息列表
+val T_TASK = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/todayTask.json"//今天任务
+val T_ABNORMAL = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/abnormalShift.json"//交班——异常信息https://github.com/wwan12/AnalogInterface/blob/master/abnormalShift.json
+val T_HANDOVERINFO = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/shiftContent.json"//交班——交班信息https://github.com/wwan12/AnalogInterface/blob/master/shiftContent.json
+val OVER_TASK = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/completeTheNursing.json"//完成任务https://github.com/wwan12/AnalogInterface/blob/master/completeTheNursing.json
+val TOMORROW_TASK = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/tomorrowTask.json"//明日任务https://github.com/wwan12/AnalogInterface/blob/master/tomorrowTask.json
+val ABNORMALITY = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/completeTheNursing.json"//异常情况提交
+val THIS_TIME_TASK = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/nursingContent.json"//某一时段护理任务
+val T_HANDOVERDIRECTOR = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/supervisorSummary.json"//交班-主管https://github.com/wwan12/AnalogInterface/blob/master/supervisorSummary.json
+val AUTO_UPDATE_MSG="https://raw.githubusercontent.com/wwan12/AnalogInterface/master/isNewMsg.json"//新消息https://github.com/wwan12/AnalogInterface/blob/master/isNewMsg.json
+val MSGLIST_READ = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/completeTheNursing.json"//消息——读
+
+val ROOM_LIST = "https://raw.githubusercontent.com/wwan12/AnalogInterface/master/roomList.json"//房间列表
+val OLDMAN_INFO="https://raw.githubusercontent.com/wwan12/AnalogInterface/master/InformationOfTheOldMan.json"//老人信息https://github.com/wwan12/AnalogInterface/blob/master/InformationOfTheOldMan.json
